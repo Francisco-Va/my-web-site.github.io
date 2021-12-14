63273e50dc6f9864ad4e4eb78136fc8ef7d02243
Francisco Vargas <79943027+Francisco-Va@users.noreply.github.com>
2021-12-14 02:02:09 -0600

    Add files via upload

---
+++ notas.js
@@ -0,0 +1,15 @@
+// cv-maquetacion/portafolio-virtual
+
+
+
+
+
+// maquetacion-css-avanzado/portafolio-cv
+
+//https://github.com/Francisco-Va/maquetacion-css-js/portafolio-cv
+//https://Francisco-Va.github.io-maquetacion-css-js/portafolio-cv
+
+
+// 6:15: 09 https://www.youtube.com/watch?v=ErtR07GLq54
+
+
